28529a808747b0ae90c512d5c29911e1076b7ba2
uller91 <girnikis@gmail.com>
2025-02-09 12:33:58 -0800

    added tests to test images, links and text_to_textnode functional

--- src/main.py
+++ src/main.py
@@ -9,40 +9,4 @@ def main():
     #text = "test ![text test](link/abc) ![text test 2](link/abc2)"
     #print(extract_markdown_images(text))
     
-    """node = TextNode("text", TextType.ITALIC)
-    print(split_nodes_image([node]))
-
-    node2 = TextNode("text", TextType.TEXT)
-    print(split_nodes_image([node2]))
-
-    node3 = TextNode("test lala![text test](link/abc) lala", TextType.TEXT)
-    print(split_nodes_image([node3]))
-
-    node4 = TextNode("test lala![text test](link/abc) ![text test 2](link/abc2) lala", TextType.TEXT)
-    print(split_nodes_image([node4]))
-
-    node5 = TextNode("![text test](link/abc) ![text test 2](link/abc2) lala", TextType.TEXT)
-    print(split_nodes_image([node5]))
-
-    node6 = TextNode("![text test](link/abc) ![text test 2](link/abc2)", TextType.TEXT)
-    print(split_nodes_image([node6]))
-
-    node7 = TextNode("![text test](link/abc)![text test 2](link/abc2)", TextType.TEXT)
-    print(split_nodes_image([node7]))
-
-    node8 = TextNode("[text test](link/abc)[text test 2](link/abc2)", TextType.TEXT)
-    print(split_nodes_link([node8]))
-    
-    node9 = TextNode("[text test](link/abc) ![text test 2](link/abc2) **bold!**", TextType.TEXT)
-    print(split_nodes_link([node9]))
-
-    nodes10 = split_nodes_link([node9])
-    print(split_nodes_image(nodes10))
-
-    nodes11 = split_nodes_image(nodes10)
-    print(split_nodes_delimiter(nodes11, "**", TextType.BOLD))"""
-
-    text1 = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    print(text_to_textnodes(text1))
-
 main()
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType, text_node_to_html_node
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 
 
 class TestDelimeter(unittest.TestCase):
@@ -137,6 +137,92 @@ class TestExtractLinkAndImage(unittest.TestCase):
         self.assertListEqual([("text", "")], result)
         self.assertListEqual([("text2", "")], result_2)
 
+
+class TestSplitImageAndLink(unittest.TestCase):
+
+    def test_no_image(self):
+        node = TextNode("text", TextType.ITALIC)
+        lst = split_nodes_image([node])
+
+        node2 = TextNode("text", TextType.TEXT)
+        lst2 = split_nodes_image([node2])
+
+        self.assertListEqual([TextNode("text", TextType.ITALIC)], lst)
+        self.assertListEqual([TextNode("text", TextType.TEXT)], lst2)
+
+    def test_image(self):
+        node3 = TextNode("test lala![text test](link/abc) lala", TextType.TEXT)
+        lst3 = split_nodes_image([node3])
+
+        node4 = TextNode("test lala![text test](link/abc) ![text test 2](link/abc2) lala", TextType.TEXT)
+        lst4 = split_nodes_image([node4])
+
+        node5 = TextNode("![text test](link/abc) ![text test 2](link/abc2) lala", TextType.TEXT)
+        lst5 = split_nodes_image([node5])
+
+        node6 = TextNode("![text test](link/abc) ![text test 2](link/abc2)", TextType.TEXT)
+        lst6 = split_nodes_image([node6])
+
+        node7 = TextNode("![text test](link/abc)![text test 2](link/abc2)", TextType.TEXT)
+        lst7 = split_nodes_image([node7])
+    
+        self.assertListEqual([TextNode("test lala", TextType.TEXT),
+                              TextNode("text test", TextType.IMAGE, "link/abc"),
+                              TextNode(" lala", TextType.TEXT)], lst3)
+        
+        self.assertListEqual([TextNode("test lala", TextType.TEXT),
+                              TextNode("text test", TextType.IMAGE, "link/abc"),
+                              TextNode(" ", TextType.TEXT),
+                              TextNode("text test 2", TextType.IMAGE, "link/abc2"),
+                              TextNode(" lala", TextType.TEXT)], lst4)
+        
+        self.assertListEqual([TextNode("text test", TextType.IMAGE, "link/abc"),
+                              TextNode(" ", TextType.TEXT),
+                              TextNode("text test 2", TextType.IMAGE, "link/abc2"),
+                              TextNode(" lala", TextType.TEXT)], lst5)
+        
+        self.assertListEqual([TextNode("text test", TextType.IMAGE, "link/abc"),
+                              TextNode(" ", TextType.TEXT),
+                              TextNode("text test 2", TextType.IMAGE, "link/abc2")], lst6)
+        
+        self.assertListEqual([TextNode("text test", TextType.IMAGE, "link/abc"),
+                              TextNode("text test 2", TextType.IMAGE, "link/abc2")], lst7)
+    
+    def test_image(self):
+        node8 = TextNode("[text test](link/abc)[text test 2](link/abc2)", TextType.TEXT)
+        lst8 = split_nodes_link([node8])
+    
+        self.assertListEqual([TextNode("text test", TextType.LINK, "link/abc"),
+                              TextNode("text test 2", TextType.LINK, "link/abc2")], lst8)
+        
+    def test_link_to_image_to_bold(self):
+        node9 = TextNode("[text test](link/abc) ![text test 2](link/abc2) **bold!**", TextType.TEXT)
+        nodes10 = split_nodes_link([node9])
+        nodes11 = split_nodes_image(nodes10)
+        lst9 = split_nodes_delimiter(nodes11, "**", TextType.BOLD)
+    
+        self.assertListEqual([TextNode("text test", TextType.LINK, "link/abc"),
+                              TextNode(" ", TextType.TEXT),
+                              TextNode("text test 2", TextType.IMAGE, "link/abc2"),
+                              TextNode(" ", TextType.TEXT),
+                              TextNode("bold!", TextType.BOLD)], lst9)
+
+
+class TestTextToTextnodes(unittest.TestCase):
+    
+    def test_text_to_textnode(self):
+        text1 = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        lst = text_to_textnodes(text1)
+        self.assertListEqual([TextNode("This is ", TextType.TEXT),
+                              TextNode("text", TextType.BOLD),
+                              TextNode(" with an ", TextType.TEXT),
+                              TextNode("italic", TextType.ITALIC),
+                              TextNode(" word and a ", TextType.TEXT),
+                              TextNode("code block", TextType.CODE),
+                              TextNode(" and an ", TextType.TEXT),
+                              TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                              TextNode(" and a ", TextType.TEXT),
+                              TextNode("link", TextType.LINK, "https://boot.dev")], lst)
 
 
 if __name__ == "__main__":
