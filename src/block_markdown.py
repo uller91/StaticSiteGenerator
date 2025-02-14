5f8e19a8e8de8a57dccc8343184941fd453f052b
uller91 <girnikis@gmail.com>
2025-02-13 18:51:46 -0800

    added block_to_block_type. Need to move tests from main to corresponding tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -10,3 +10,57 @@ def markdown_to_blocks(markdown):
         if item != "":
             return_list.append(item)
     return return_list
+
+def block_to_block_type(block):
+    if block[:2] == "# ":
+        return "heading 1"
+    elif block[:3] == "## ":
+        return "heading 2"
+    elif block[:4] == "### ":
+        return "heading 3"
+    elif block[:5] == "#### ":
+        return "heading 4"
+    elif block[:6] == "##### ":
+        return "heading 5"
+    elif block[:7] == "###### ":
+        return "heading 6"
+    elif block[:3] == "```" and block[-3:] == "```":
+        return "code"
+    
+    lines = block.split("\n")
+    counter = 0
+    if lines[0][0] == ">":
+        for line in lines:
+            if line[0] == ">":
+                counter += 1
+        if counter == len(lines):
+            return "quote"    
+        
+    if lines[0][:2] == "* ":
+        for line in lines:
+            if line[:2] == "* ":
+                counter += 1
+        if counter == len(lines):
+            return "unordered_list"    
+    if lines[0][:2] == "- ":
+        for line in lines:
+            if line[:2] == "- ":
+                counter += 1
+        if counter == len(lines):
+            return "unordered_list"    
+        
+    if lines[0][:3] == "1. ":
+        i = 1
+        for line in lines:
+            if line[:3] == f"{i}. ":
+                counter += 1
+                i += 1
+        if counter == len(lines):
+            return "ordered_list"    
+    
+    return "normal"
+
+    
+    
+    
+    
--- src/main.py
+++ src/main.py
@@ -3,14 +3,33 @@ import re
 from textnode import TextNode, TextType, text_node_to_html_node
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 
 def main():
-    print("hello")
-    text = "text\n\n \n\ntext2 \ntext3 \n\n* text4  \n\n\ntext5\n\n\n\ntext6"
-    list = markdown_to_blocks(text)
-    print(list)
-    return
+    #print("hello")
+    #text = "text\n\n \n\ntext2 \ntext3 \n\n* text4  \n\n\ntext5\n\n\n\ntext6"
+    #list = markdown_to_blocks(text)
+    #print(list)
+    
+    text_2 = "###### heading"
+    print(block_to_block_type(text_2))
+
+    text_3 = "> quote\n> quote\n> qq"
+    print(block_to_block_type(text_3))
+
+    text_4 = "``` code ```"
+    print(block_to_block_type(text_4))
+
+    text_5 = "* list\n* list"
+    print(block_to_block_type(text_5))
+
+    text_6 = "- list\n- list"
+    print(block_to_block_type(text_6))
+
+    text_7 = "1. list\n2. list"
+    print(block_to_block_type(text_7))
+
+
 
 main()
