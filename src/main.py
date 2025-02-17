548bace2e7ac87e046c76bd4fed3b773d59d1a47
uller91 <girnikis@gmail.com>
2025-02-17 13:00:17 -0800

    added static to public folder generator

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from block_markdown import *
+from static_to_public import *
 
 
 def main():
-    text = "hello world!"
+    path_public = "./public"
+    path_static = "./static"
+    files_copied = static_to_public(path_public, path_static, [])
+    #print(files_copied)
 
 main()
